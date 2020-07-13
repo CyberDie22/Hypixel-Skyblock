de403054834210219d929c7a58baa2e4a69b7d3b
CyberDie22 <250144benjamin@gmail.com>
2020-07-12 18:50:05 -0700

    Inital commit, setting up project

---
+++ Spigot 1.13.2/src/main/java/me/cyberdie22/hyskyblock/main.java
@@ -0,0 +1,17 @@
+package me.cyberdie22.hyskyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
